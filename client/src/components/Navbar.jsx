334d5fcd99dc8a02b3465312802ce9fe3fd722e2
Amaan Pasha <Amaanpasha23@gmail.com>
2026-04-22 16:08:16 +0530

    new Navbar is added in best quality over here

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,39 @@
+import { Link, useNavigate } from "react-router-dom";
+import "../styles/Navbar.css";
+
 function Navbar() {
+    const navigate = useNavigate();
+
+    const token = localStorage.getItem("token");
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        navigate("/login");
+    };
+
     return (
-        <div>
-            Navbar Working
-        </div>
+        <nav className="navbar">
+            <div className="navbar-container">
+                <h2 className="logo">MiniTask</h2>
+
+                <div className="nav-links">
+                    {!token ? (
+                        <>
+                            <Link to="/register">Register</Link>
+                            <Link to="/login">Login</Link>
+                        </>
+                    ) : (
+                        <>
+                            <Link to="/dashboard">Dashboard</Link>
+                            <Link to="/tasks">Tasks</Link>
+                            <button className="logout-btn" onClick={handleLogout}>
+                                Logout
+                            </button>
+                        </>
+                    )}
+                </div>
+            </div>
+        </nav>
     );
 }
 
